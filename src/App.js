9e261dd4421e3a20936975203c69be851098decd
cwest9400 <chrstphrwest@gmail.com>
2023-02-20 15:58:22 -0500

    tailwind setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 
-import './App.css';
+
 
 function App() {
   return (
     <div>
-      <h1>sketch daily</h1>
+      <h1 className="text-3xl font-bold underline">sketch daily</h1>
     </div>
   );
 }
