dfe7233e3d63d3cbda53442421a0e60530a004a7
cwest9400 <chrstphrwest@gmail.com>
2023-02-20 18:29:42 -0500

    tailwind installed

--- src/App.js
+++ src/App.js
@@ -6,7 +6,14 @@ function App() {
   return (
     <div>
       <Header />
-      <h1 className="text-3xl font-bold underline">sketch daily</h1>
+      <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+      <p className="text-3xl text-gray-700 font-bold mb-5">
+        Welcome!
+      </p>
+      <p className="text-gray-500 text-lg">
+        React and Tailwind CSS in action
+      </p>
+    </div>
       <Footer />
     </div>
   );
---
+++ src/pages/home.js
@@ -0,0 +1,11 @@
+
+export default function Home(){
+    
+    return (
+        <div>
+            <p>
+                content yay
+            </p>
+        </div>
+    )
+}
