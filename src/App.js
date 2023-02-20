b7ed91f3534a8d8b7d89d50db47fa27e4eb3e191
cwest9400 <chrstphrwest@gmail.com>
2023-02-20 16:03:28 -0500

    basic footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-
+import Header from "./components/header";
 
 
 function App() {
   return (
     <div>
+      <Header />
       <h1 className="text-3xl font-bold underline">sketch daily</h1>
     </div>
   );
---
+++ src/components/footer.js
@@ -0,0 +1,7 @@
+function Footer (props) {
+    <div>
+        <h1>Footer</h1>
+    </div>
+}
+
+export default Footer
